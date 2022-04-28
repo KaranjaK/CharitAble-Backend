12562f85d4ff0a9a866f1cf4de8961cca3f66803
vicky <victoria.awuor@student.moringaschool.com>
2022-04-28 14:50:23 +0300

    udated models

--- charitable/models.py
+++ charitable/models.py
@@ -4,16 +4,24 @@ from django.contrib.auth.models import User
 from django.contrib.postgres.fields import RangeField
 
 # Create your models here.
+
+class Requests(models.Model):
+    fin_statements = models.FileField(upload_to='documents/%Y/%m/%d')
+    reports = models.FileField(upload_to='documents')
+    amount_range = RangeField()
+    verification = models.BooleanField()
+    
 class NGO(models.Model):
     name = models.TextField(max_length=50)
     logo = CloudinaryField('images', null=True)
     category = models.TextField()
     description = models.TextField(max_length=200)
     web_link = models.URLField(max_length=300)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='project')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
     contact = models.IntegerField(default=0,blank=True, null=True)
     email = models.EmailField(max_length=100, null=True)
     location = models.CharField(max_length=100)
+    request = models.ForeignKey(Requests, on_delete=models.CASCADE, related_name='request')
 
 
 class Donor(models.Model):
@@ -23,13 +31,9 @@ class Donor(models.Model):
     web_link = models.URLField(max_length=300)
     contact = models.IntegerField(default=0,blank=True, null=True)
     email = models.EmailField(max_length=100, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='project')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
     location = models.CharField(max_length=100)
-
-class Requests(models.Model):
-    fin_statements = models.FileField(upload_to='documents/%Y/%m/%d')
-    reports = models.FileField(upload_to='documents')
-    amount_range = RangeField()
+    request = models.ForeignKey(Requests, on_delete=models.CASCADE, related_name='request')
 
 
 
