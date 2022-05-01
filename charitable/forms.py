114db3ffc401f0d4429f7870bfc4ce4b92c3f49b
kojwangbora <kojwangbora254@gmail.com>
2022-05-01 15:01:12 +0300

    add signup form

--- charitable/forms.py
+++ charitable/forms.py
@@ -18,3 +18,37 @@ class LoginForm(forms.Form):
 
         )
     )
+
+class SignUpForm(UserCreationForm):
+    username= forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class":"form-control"
+            }
+        )
+    )
+    password1= forms.CharField(
+        widget= forms.PasswordInput(
+            attrs={
+                "class":"form-control"
+            }
+        )
+    )
+    password2= forms.CharField(
+        widget= forms.PasswordInput(
+            attrs={
+                "class":"form-control"
+            }
+        )
+    )
+    email= forms.CharField(
+        widget= forms.TextInput(
+            attrs={
+                "class":"form-control"
+            }
+        )
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', 'is_admin', 'is_NGO', 'is_Donor')
