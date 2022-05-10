5d06ee6cfa9d8cfd5e687992d3d65cb952f6db5a
kojwangbora <kojwangbora254@gmail.com>
2022-05-11 00:09:27 +0300

    modilfy settings.py

--- Charity/settings.py
+++ Charity/settings.py
@@ -19,7 +19,7 @@ import cloudinary.api
 
 from decouple import config,Csv
 import os
-from decouple import config,Csv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
